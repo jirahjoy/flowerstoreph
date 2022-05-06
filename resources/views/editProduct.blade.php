b2e04ccad93526d0a1c407003eee5e7fd6137d46
jirahjoy <jirahmedina@gmail.com>
2022-05-06 17:45:27 +0800

    add addProduct and editProduct views and modify index view

--- resources/views/editProduct.blade.php
+++ resources/views/editProduct.blade.php
@@ -16,7 +16,7 @@
 </style>
 
 <div class="card push-top">
-    <h5 class='mx-3 mt-3'>Add Product</h5>
+    <h5 class='mx-3 mt-3'>Edit Product</h5>
     <div class="card-body">
                      @if($errors->any())
                                 <div class="alert alert-danger">
@@ -27,35 +27,33 @@
                                     </ul>
                                 </div>
                      @endif
-                           
-                                                    
+                             
+                                             
 
-                            <form action="{{route('products.store')}}" method="POST" id='modalForm'>
+                            <form action="{{route('products.update', $products->id)}}" method="POST" >
                                 <!-- Cross Site Request Forgery-->
                                 @csrf
+                                @method('PATCH')
                                 <div class="form-group">
                                     <label for="productName">Product Name:</label>
-                                    <input type="text" class="form-control" name="product_name" >
+                                    <input type="text" class="form-control" name="product_name" id='productName' value='{{$products->product_name}}'>
                                 </div>
                                 <div class="form-group">
                                     <label for="productDescription">Product Description:</label>
-                                    <input type="text" class="form-control" name="product_description" >
+                                    <input type="text" class="form-control" name="product_description" id='productDescription' value='{{$products->product_description}}'>
                                 </div>
                                 <div class="form-group">
                                     <label for="quantity">Quantity:</label>
-                                    <input type="number" class="form-control" name="quantity" >
+                                    <input type="number" class="form-control" name="quantity" id='quantity' value='{{$products->quantity}}'>
                                 </div>
                                 <div class="form-group">
                                     <label for="price">Price:</label>
-                                    <input type="text" class="form-control" name="price" >
+                                    <input type="text" class="form-control" name="price" id='price' value='{{$products->price}}'>
                                 </div>
-                                <input class="btn btn-primary mt-3" type='submit' name='submit' value='Add Product' id='btn-submit'>
+                                <input class="btn btn-primary mt-5" type='submit' name='submit' value='Edit Product' id='btn-submit'>
                     </form>
 
     </div>
 </div>
-
-
-
 
 @endsection()
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,60 +2,135 @@
 <!-- call the yield -->
 @section('content')
 
-<ul class="nav nav-tabs mt-5">
-  <li class="nav-item">
-    <a class="nav-link active" aria-current="page" href="index.php">Products</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="addproduct">Order</a>
-  </li>
-  
-</ul>
+<style>
+    
+    .push-top{
+        margin-top: 50px;
+    }
+    .form-group{
+        margin-top: 5px;
+    }
 
-<table class='table table-striped mt-5'>
-    <thead>
-    <tr>
-        <th>Product Name</th>
-        <th>Product Description</th>
-        <th>Price</th>
-    </tr>
-    </thead>
+</style>
 
-    <tbody>
-    <tr>
-        <td>Bouquet</td>
-        <td>12 roses</td>
-        <td>2000</td>  
-    </tr>
-    <tr>
-        <td>Bouquet</td>
-        <td>12 roses</td>
-        <td>2000</td>  
-    </tr>
-    </tbody>
-</table>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+        <nav>
+            <div class="nav nav-tabs" id="nav-tab" role="tablist">
+                <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Home</button>
+                <button class="nav-link" id="nav-product-tab" data-bs-toggle="tab" data-bs-target="#nav-product" type="button" role="tab" aria-controls="nav-product" aria-selected="false">Product</button>
+                <button class="nav-link" id="nav-order-tab" data-bs-toggle="tab" data-bs-target="#nav-order" type="button" role="tab" aria-controls="nav-order" aria-selected="false">Order</button>
+            </div>
+        </nav>
 
-<table class='table table-striped mt-5'>
-    <thead>
-    <tr>
-        <th>Order ID</th>
-        <th>Product Name</th>
-        <th>Price</th>
-    </tr>
-    </thead>
+    
 
-    <tbody>
-    <tr>
-        <td>1</td>
-        <td>roses</td>
-        <td>2000</td>  
-    </tr>
-    <tr>
-        <td>2</td>
-        <td>roses</td>
-        <td>2000</td>  
-    </tr>
-    </tbody>
-</table>
+        <div class="tab-content" id="nav-tabContent">
+            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+            <!-- Button trigger modal -->
+            <a href="/addproduct" class="btn btn-primary mt-5" >
+            Add Product
+            </a>
+
+            <table class='table table-striped mt-5'>
+                <thead>
+                <tr>
+                    <th>Product ID</th>
+                    <th>Product Name</th>
+                    <th>Product Description</th>
+                    <th>Quantity</th>
+                    <th>Price</th>
+                    <th>Edit</th>
+                    <th>Status</th>
+                </tr>
+                </thead>
+
+                <tbody>
+                @foreach($products as $product)
+                <tr>
+                    <td>{{$product->id}}</td>
+                    <td>{{$product->product_name}}</td>
+                    <td>{{$product->product_description}}</td>
+                    <td>{{$product->quantity}}</td>
+                    <td>&#8369;{{$product->price}}</td>
+                    <td>
+                    <a href="{{route('products.edit', $product->id)}}" class="btn btn-primary btn-sm" >
+            Edit
+            </a>
+                        
+    
+                    </td>
+                    <td>
+                        <a href="/products/status/{{$product->id}} " class=" text-center btn-sm btn {{($product->status == 0) ? 'btn-danger' : 'btn-primary'}}"> {{($product->status == 0) ? 'Enable' : 'Disable'}}</a>
+                        
+                        
+                    </td>
+                </tr>
+                @endforeach
+                
+                </tbody>
+            </table>
+            </div>
+            <div class="tab-pane fade" id="nav-product" role="tabpanel" aria-labelledby="nav-product-tab">
+
+                <table class='table table-striped mt-5'>
+                    <thead>
+                    <tr>
+                        <th>Product Name</th>
+                        <th>Product Description</th>
+                        <th>Price</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    @foreach($productsAvail as $productAvailable)
+                    <tr>
+                        <td>{{$productAvailable->product_name}}</td>
+                        <td>{{$productAvailable->product_description}}</td>
+                        <td>&#8369;{{$productAvailable->price}}</td>
+                    </tr>
+                     @endforeach
+                     <tr>
+                         <th colspan='3' class='text-left'>Total Products: {{$productsAvail->count()}}</th>
+                         
+                     </tr>
+                    </tbody>
+                </table>
+            </div>
+            <div class="tab-pane fade" id="nav-order" role="tabpanel" aria-labelledby="nav-order-tab">
+
+                <table class='table table-striped mt-5'>
+                    <thead>
+                    <tr>
+                        <th>Order ID</th>
+                        <th>Product Name</th>
+                        <th>Price</th>
+                    </tr>
+                    </thead>
+
+                    <tbody>
+                    @foreach($orders as $order)
+                    <tr>
+                        <td>{{$order->id}}</td>
+                        <td>{{$order->product_name}}</td>
+                        <td>&#8369;{{$order->price}}</td>
+                         
+                    </tr>
+                    @endforeach
+                    <tr>
+                         <th colspan='3' class='text-left'>Total Orders: {{$orders->count()}}</th>
+                         
+                     </tr>
+                    
+                    </tbody>
+                </table>
+            </div>
+        </div>
+	</div>
+</div>
+
+
+
 
 @endsection()
